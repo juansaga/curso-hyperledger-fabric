765b33623a1e9dfd56997e262e9103581dc2f8b3
Juan M. Sanchez <juansaga00@gmail.com>
2022-10-18 17:58:31 -0500

    chaincode basico

--- chaincode/foodcontrol/foodcontrol.go
+++ chaincode/foodcontrol/foodcontrol.go
@@ -1,23 +1,68 @@
 package main
 
 import (
-
 	"encoding/json"
 	"fmt"
-	"strconv"
+
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
- type SmartContract struct (
+type SmartContract struct {
 	contractapi.Contract
-)
+}
 
-func main(){
+//Descripcion del activo
+
+type Food struct {
+	Farmer  string `json:"farmer"`
+	Variety string `json:"variety"`
+}
+
+func (s *SmartContract) Set(ctx contractapi.TransactionContextInterface, foodId string, farmer string, variety string) error {
+
+	// validaciones, que no exista el food id
+
+	food := Food{
+		Farmer:  farmer,
+		Variety: variety,
+	}
+
+	foodAsBytes, err := json.Marshal(food)
+	if err != nil {
+		fmt.Printf("Marshal error: %s", err.Error())
+		return err
+	}
+
+	return ctx.GetStub().PutState(foodId, foodAsBytes)
+}
+
+func (s *SmartContract) Query(ctx contractapi.TransactionContextInterface, foodId string) (*Food, error) {
+
+	foodAsBytes, err := ctx.GetStub().GetState(foodId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from world  state %s", err.Error())
+	}
+
+	if foodAsBytes == nil {
+		return nil, fmt.Errorf("%s does not exist", foodId)
+	}
+
+	food := new(Food)
+
+	err = json.Unmarshal(foodAsBytes, food)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal error %s", err.Error())
+	}
+
+	return food, nil
+}
+
+func main() {
 	chaincode, err := contractapi.NewChaincode(new(SmartContract))
 
-	if err != nil{
+	if err != nil {
 		fmt.Printf("Error create foodcontrol chaincode %s", err.Error())
-		return 
+		return
 	}
 
 	if err := chaincode.Start(); err != nil {
